3cc2be49788e7c6c97fb0dfff69daa6b492d9424
Tom Kelley <th.kelley@northeastern.edu>
2024-01-11 11:21:29 -0500

    Add files via upload

---
+++ program_template.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on ...
+
+@author: ...
+Description: ...
+    
+"""
+
+def main():
+    #Program body
+    print('Some output')
+    return 0
+    
+#The code below runs whatever is in main()
+if __name__ == '__main__':
+    main()
